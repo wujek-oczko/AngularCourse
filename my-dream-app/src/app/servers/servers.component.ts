0d03db1bc155205015e365775db25a83fab00148
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-22 14:31:43 +0200

    One way data binding

--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -23,4 +24,8 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created';
   }
 
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
