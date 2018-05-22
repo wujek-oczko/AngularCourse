dc6e0791392f14a55c2acfc3ac34f200247fd06c
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-22 16:02:09 +0200

    Basic ngIf usage

--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server created';
   serverName = 'Lalala';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: Event) {
