8f0208ef2bae8d3c24aa58ac9e5753770dc6c57f
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-22 16:19:30 +0200

    ngStyle directive introduction

--- my-dream-app/src/app/server/server.component.ts
+++ my-dream-app/src/app/server/server.component.ts
@@ -8,8 +8,16 @@ export class ServerComponent {
   serverID: number = 10;
   serverStatus: string = 'dead';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'alive!!' : 'dead :(';
+    this.serverID = Math.round(Math.random() * 10);
+  }
+
   getServerStatus() {
     return this.serverStatus;
   }
 
+  getColor() {
+    return this.serverStatus === 'alive!!' ? 'green' : 'red';
+  }
 }
