04bf8d3917a32ae9aec70ffd6cdb863073a3b750
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-22 13:32:02 +0200

    String interpolation

--- my-dream-app/src/app/server/server.component.ts
+++ my-dream-app/src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = 'dead';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
 }
