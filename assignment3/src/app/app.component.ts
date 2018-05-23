f1b82f9e69bae89ac31a33b00b4f53ec9765ff1f
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-23 10:13:41 +0200

    Assignment3

--- assignment3/src/app/app.component.ts
+++ assignment3/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayParagraph: boolean = false;
+  clicks = [];
+  isGreater: boolean = false;
+
+  toggleParagraph(event: Event) {
+    console.log(event);
+    this.clicks.push(new Date().toLocaleTimeString());
+    this.displayParagraph = !this.displayParagraph;
+    if (this.clicks.length > 5) {
+      this.isGreater = true;
+    }
+  }
+
+  getBackground() {
+    return this.isGreater === true ? 'blue' : 'white';
+  }
 }
