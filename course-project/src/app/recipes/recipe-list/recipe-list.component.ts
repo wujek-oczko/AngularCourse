5ced4c5ae652a2b63735574c139b12d76a8e9327
Marcin Jachymek <marcin.jachymek@blue-veery.com>
2018-05-23 13:11:51 +0200

    Displaying recipes

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: [ './recipe-list.component.css' ]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'Test description', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+  ];
 
   constructor () {
   }
